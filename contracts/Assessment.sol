e6c60d2c0714dda1a5140c4cc159b072bb04a782
Abero Geronga (Blake) <gerongaabero@gmail.com>
2024-07-19 21:11:02 +0800

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -2,50 +2,52 @@
 pragma solidity ^0.8.9;
 
 contract Assessment {
+    // State variables
     address payable public owner;
     uint256 public balance;
 
+    // Events
     event Deposit(uint256 amount, uint256 timestamp);
     event Withdraw(uint256 amount, uint256 timestamp);
     event WithdrawAll(uint256 amount, uint256 timestamp);
 
+    // Structs
     struct Transaction {
         string action;
         uint256 amount;
         uint256 timestamp;
     }
 
+    // State variables for transaction history
     Transaction[] public transactionHistory;
 
-    constructor(uint initBalance) payable {
+    // Constructor
+    constructor(uint256 initBalance) payable {
         owner = payable(msg.sender);
         balance = initBalance;
     }
 
+    // Public functions
     function getBalance() public view returns (uint256) {
         return balance;
     }
 
     function deposit(uint256 _amount) public payable {
-        require(msg.sender == owner, "You are not the owner of this account");
+        require(msg.sender == owner, "You are not the owner of this account!");
 
-        uint _previousBalance = balance;
-
+        uint256 _previousBalance = balance;
         balance += _amount;
 
         assert(balance == _previousBalance + _amount);
 
         transactionHistory.push(Transaction("Deposit", _amount, block.timestamp));
-
         emit Deposit(_amount, block.timestamp);
     }
 
-    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
-
     function withdraw(uint256 _withdrawAmount) public {
-        require(msg.sender == owner, "You are not the owner of this account");
+        require(msg.sender == owner, "You are not the owner of this account!");
 
-        uint _previousBalance = balance;
+        uint256 _previousBalance = balance;
         if (balance < _withdrawAmount) {
             revert InsufficientBalance({
                 balance: balance,
@@ -54,30 +56,29 @@ contract Assessment {
         }
 
         balance -= _withdrawAmount;
-
         assert(balance == (_previousBalance - _withdrawAmount));
 
         transactionHistory.push(Transaction("Withdraw", _withdrawAmount, block.timestamp));
-
         emit Withdraw(_withdrawAmount, block.timestamp);
     }
 
     function withdrawAll() public {
-        require(msg.sender == owner, "You are not the owner of this account");
+        require(msg.sender == owner, "You are not the owner of this account!");
 
-        uint _previousBalance = balance;
-        uint _withdrawAmount = balance;
-
+        uint256 _previousBalance = balance;
+        uint256 _withdrawAmount = balance;
         balance = 0;
 
         assert(balance == 0);
 
         transactionHistory.push(Transaction("Withdraw All", _withdrawAmount, block.timestamp));
-
         emit WithdrawAll(_withdrawAmount, block.timestamp);
     }
 
     function getTransactionHistory() public view returns (Transaction[] memory) {
         return transactionHistory;
     }
+
+    // Custom errors
+    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 }
